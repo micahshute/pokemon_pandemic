d972b0de64c1e90a049f792ec9396856d6854848
Micah Shute <micah.shute@gmail.com>
2020-04-07 15:19:32 -0700

    initial commit

---
+++ lib/pokemon_pandemic.rb
@@ -0,0 +1,6 @@
+require "pokemon_pandemic/version"
+
+module PokemonPandemic
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/pokemon_pandemic/version.rb
@@ -0,0 +1,3 @@
+module PokemonPandemic
+  VERSION = "0.1.0"
+end
